b549b6af04932232e050c4e3ce1d601b9488ab2f
Sajid Rahman <sajidjrahman@icloud.com>
2022-03-10 22:09:19 +0100

    fixed multiple posts

--- js/app.js
+++ js/app.js
@@ -35,10 +35,12 @@ const switchTab = (id) => {
     document.getElementById("posts").style.display = "grid";
     document.getElementById("liked").style.display = "none";
     document.getElementById("reported").style.display = "none";
+    document.getElementById("reported").innerHTML = ""
   } else if (id === "liked") {
     document.getElementById("liked").style.display = "block";
     document.getElementById("posts").style.display = "none";
     document.getElementById("reported").style.display = "none";
+    document.getElementById("liked").innerHTML = "";
 
     displayLikedPosts();
   } else {
